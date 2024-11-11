74b3933f139b8e8e60501cab73dca36c655d29eb
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2024-11-11 21:24:00 +0700

    Fetch and display products in HomeScreen with FlatList and navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../store/store";
+import axios from "axios";
 
-const HomeScreen = () => {
+import { NavigationProp } from "@react-navigation/native";
+
+export default function HomeScreen({
+  navigation,
+}: {
+  navigation: NavigationProp<any>;
+}) {
+  const products = useSelector((state: { products: any[] }) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get("https://api.example.com/products")
+      .then((response) => dispatch(setProducts(response.data)))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>{item.name}</Text>
+            <Button
+              title="Details"
+              onPress={() =>
+                navigation.navigate("Details", { productId: item.id })
+              }
+            />
+          </View>
+        )}
+      />
     </View>
   );
-};
+}
 
-export default HomeScreen;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+});
