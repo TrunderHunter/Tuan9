f1be7ed679a0fce0e1999a796e9b06e2a55f1677
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2024-11-11 21:26:04 +0700

    Add Redux store and product management slice with actions

---
+++ src/store/store.js
@@ -0,0 +1,28 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState: [],
+  reducers: {
+    setProducts: (state, action) => action.payload,
+    addProduct: (state, action) => {
+      state.push(action.payload);
+    },
+    updateProduct: (state, action) => {
+      const index = state.findIndex((p) => p.id === action.payload.id);
+      if (index !== -1) state[index] = action.payload;
+    },
+    deleteProduct: (state, action) => {
+      return state.filter((p) => p.id !== action.payload);
+    },
+  },
+});
+
+export const { setProducts, addProduct, updateProduct, deleteProduct } =
+  productsSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    products: productsSlice.reducer,
+  },
+});
